2dfd585a79cad3248cb2b0546c3cf0978df7c29c
Vaio <mauricio.monteiro@aluno.faculdadeimpacta.com.br>
2026-05-27 22:06:03 -0300

    Introdução ao nível Intermediário

--- src/NivelBasico/Array.java
+++ src/NivelBasico/Array.java
@@ -1,5 +1,7 @@
 package NivelBasico;
 
+import java.sql.SQLOutput;
+
 public class Array {
 
     public static void main(String[] args) {
@@ -61,6 +63,27 @@ public class Array {
         //Diferença de Array pra Lista
         //Araay sempre vai ter um tipo definido, tamanho fixo
         //As listas tem tamanho flexiciveis
+
+
+        //Introdução de Arrays multidimensionais / Arrays 2D
+        String[][] ninjasEAldeias = new String[3][3];
+
+        ninjasEAldeias [0][0] = "Konaha";
+        ninjasEAldeias [0][1] = "Naruto Uzumaki";
+        ninjasEAldeias [0][2] = "Sasuke Uchiha";
+
+        ninjasEAldeias [1][0] = "Nevoa";
+        ninjasEAldeias [1][1] = "Zabuza";
+        ninjasEAldeias [1][2] = "Haku";
+
+        ninjasEAldeias [2][0] = "Deserto";
+        ninjasEAldeias [2][1] = "Gaara";
+        ninjasEAldeias [2][2] = "Temari";
+
+        for (int i = 0; i < ninjasEAldeias.length; i++) {
+            System.out.println("Aldeia: " + ninjasEAldeias[i][0] + " - Ninja 1: " + ninjasEAldeias[i][1] + " - Ninja 2: " + ninjasEAldeias[i][2]);
+        }
+
     }
 
 }
---
+++ src/NivelIntermediario/Main.java
@@ -0,0 +1,36 @@
+package NivelIntermediario;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Criar o ninja Naruto - Naruto é um OBJETO
+        Ninja Naruto = new Ninja();
+        Naruto.nome = "Naruto Uzumaki";
+        Naruto.aldeia = "Aldeia da Folha";
+        Naruto.idade = 17;
+
+        // Criar o ninja Sasuke Uchiha - Sasuke é um OBJETO
+        Ninja Sasuke = new Ninja();
+        Sasuke.nome = "Sasuke Uchiha";
+        Sasuke.aldeia = "Aldeia da Folha";
+        Sasuke.idade = 18;
+
+        // Criar Sakura Haruno - Sakura é um OBJETO
+        Ninja Sakura = new Ninja();
+        Sakura.nome = "Sakura Haruno";
+        Sakura.aldeia = "Aldeia da Folha";
+        Sakura.idade = 18;
+
+
+        // Programação Orientada a Objeto - POO
+        // 3 Pilares - Herança, Polimorfismo e Encapsulamento
+
+        // HERANÇA - Usar caracteristicas de uma classe mãe em outras classes filhas para assim poupar código e agilizar trabalho.
+        // POLIMORFISMO - Criar uma função única que pode ser utilizada em diferentes classes com diferentes caracteristicas.
+        // ENCAPSULAMENTO - Capacidade de proteger variaveis e classes para que não  haja vazamento e mantendo assim protegida e encapsulada nossos objetos.
+
+
+
+    }
+}
